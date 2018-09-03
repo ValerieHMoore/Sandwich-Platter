85578c1e84b1ae684b98f9d44b76923c69b65e8a
ValerieHMoore <periwinkle17@outlook.com>
2018-09-03 16:16:54 -0400

    set up models and controllers

--- app/models/sandwich.rb
+++ app/models/sandwich.rb
@@ -1,2 +1,3 @@
 class Sandwich < ActiveRecord::Base
+    belongs_to :user
 end
--- db/migrate/20180903190420_create_sandwiches.rb
+++ db/migrate/20180903190420_create_sandwiches.rb
@@ -1,7 +1,8 @@
 class CreateSandwiches < ActiveRecord::Migration
   def change
     create_table :sandwiches do |t|
-
+      t.string :sandwich_name
+      t.string :ingredients
       t.timestamps null: false
     end
   end
